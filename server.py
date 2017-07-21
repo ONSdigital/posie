d521a2cb2fa90d892ab82d42b95b3ba0e86ce086
Warren Bailey <warren@warrenbailey.net>
2017-07-21 15:04:50 +0100

    Initial check in of secrets store

--- server.py
+++ server.py
@@ -5,13 +5,64 @@ from structlog import wrap_logger
 import binascii
 import logging
 import os
+import yaml
+import settings
+from secrets import SecretStore
 
 from sdx.common.logger_config import logger_initial_config
 
 
 __version__ = "1.3.0"
 
+
+EXPECTED_SECRETS = [
+    "SDX_SECRET_KEY",
+]
+
+KEY_PURPOSE_SUBMISSION = 'eq-submission'
+
+
+def validate_required_submission_keys(secrets):
+    found_submission_public = False
+    found_submission_private = False
+
+    for kid in secrets['keys']:
+        key = secrets['keys'][kid]
+        if key['purpose'] == KEY_PURPOSE_SUBMISSION:
+            if key['type'] == 'public':
+                if found_submission_public:
+                    raise Exception("Multiple public submission keys loaded")
+                else:
+                    found_submission_public = True
+
+            if key['type'] == 'private':
+                if found_submission_private:
+                    raise Exception("Multiple private submission keys loaded")
+                else:
+                    found_submission_private = True
+
+    if not found_submission_public:
+        raise Exception("No public submission key loaded")
+
+    if not found_submission_private:
+        raise Exception("No private submission key loaded")
+
+
+def validate_required_secrets(secrets):
+    for required_secret in EXPECTED_SECRETS:
+        if required_secret not in secrets['secrets']:
+            raise Exception("Missing Secret [{}]".format(required_secret))
+
+    validate_required_submission_keys(secrets)
+
 app = Flask(__name__)
+app.config.from_object(settings)
+
+app.sdx = {}
+
+secrets = yaml.safe_load(open(app.config['SDX_SECRETS_FILE']))
+validate_required_secrets(secrets)
+app.sdx['secret_store'] = SecretStore(secrets)
 
 logger_initial_config(service_name='sdx-decrypt')
 
@@ -19,6 +70,7 @@ logger = wrap_logger(
     logging.getLogger(__name__)
 )
 logger.info("START", version=__version__)
+
 
 
 def get_decrypter():
@@ -108,5 +160,5 @@ def healthcheck():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT", 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
--- settings.py
+++ settings.py
@@ -10,10 +10,12 @@ def get_key(key_name):
 EQ_JWT_LEEWAY_IN_SECONDS = 120
 
 # eq keys
-EQ_PUBLIC_KEY = get_key(os.getenv('EQ_PUBLIC_KEY', "/keys/sdc-submission-signing-sr-public-key.pem"))
+EQ_PUBLIC_KEY = get_key(os.getenv('EQ_PUBLIC_KEY', "./jwt-test-keys/sdc-submission-signing-sr-public-key.pem"))
 
 # sdx keys
-PRIVATE_KEY = get_key(os.getenv('PRIVATE_KEY', "/keys/sdc-submission-encryption-sdx-private-key.pem"))
+PRIVATE_KEY = get_key(os.getenv('PRIVATE_KEY', "./jwt-test-keys/sdc-submission-encryption-sdx-private-key.pem"))
 PRIVATE_KEY_PASSWORD = os.getenv("PRIVATE_KEY_PASSWORD", "digitaleq")
 
 LOGGING_LOCATION = "logs/decrypt.log"
+
+SDX_SECRETS_FILE = os.getenv('SDX_SECRETS_FILE', 'secrets.yml')
